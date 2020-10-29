208189bc300799afdc0d421b5cf6bf55d7812d7b
sulemanfarooqi <71682194+sulemanfarooqi@users.noreply.github.com>
2020-10-28 22:26:06 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package brickbreaker;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,700,600); //set ressolution
+        obj.setTitle("Breaker Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close application on close
+        obj.add(gamePlay);
+        
+    }
+    
+}
